b5d6d19d1285d80d9ba6dea81f605b826873e314
Kawisara bunyen <kawisara@Kawisaras-MacBook-Pro.local>
2021-08-03 15:23:02 +0700

    rename

--- src/main/java/com/example/demo/WorkshopDipApplication.java
+++ src/main/java/com/example/demo/WorkshopDipApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class DemoApplication {
+public class WorkshopDipApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(WorkshopDipApplication.class, args);
 		System.out.println("Bean count= " + context.getBeanDefinitionCount());
 
 		GeneratedIdService service = context.getBean(GeneratedIdService.class);
